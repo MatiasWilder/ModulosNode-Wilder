8f23c9465c22fe26c725818c83b040c62972c631
MatiasWilder <48178322@est.ort.edu.ar>
2025-04-04 12:14:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//1
+const concatenar =  require ('./modulos/strings.js');
+console.log(concatenar("Buenas" , " que tal"))
+
+//2
+import suma from './modulos/matematica.js';
+import resta from './modulos/matematica.js';
+import multiplicacion from './modulos/matematica.js';
+import division from './modulos/matematica.js';
+
+console.log(suma(2,4));
+console.log(resta(2,4));
+console.log(multiplicacion(2,4));
+console.log(division(2,4));
+
---
+++ matematica.js
@@ -0,0 +1,11 @@
+//2
+const  suma = (num1,num2) => num1 + num2; 
+const  resta = (num1,num2) => num1 - num2; 
+const  multiplicacion = (num1,num2) => num1 * num2; 
+const  division = (num1,num2) => num1 / num2; 
+
+export default {
+    suma,
+    resta,
+    multiplicacion,
+    division};
